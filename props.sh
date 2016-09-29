91b6880b5c872284bb8e3de80852101c83e4daba
Jesse Yates <jesse@fineo.io>
2016-09-28 18:15:21 -0700

    Fixing property loading

--- props.sh
+++ props.sh
@@ -10,10 +10,10 @@ fi
 
 export e2e_tools=$WORKSPACE/tools
 export client=$WORKSPACE/client
-export client_tools_jar=$WORKSPACE/client/`ls tools*.jar| grep -v tests | grep -v jdbc`
-export client_jdbc_jar=$WORKSPACE/client/`ls tools*jdbc.jar`
+export client_tools_jar=$WORKSPACE/client/`ls $client/tools*.jar| grep -v tests | grep -v jdbc`
+export client_jdbc_jar=$WORKSPACE/client/`ls $client/tools*jdbc.jar`
 export queries=$WORKSPACE/queries
-export JOB_OUTPUT/$WORKSPACE/job
+export JOB_OUTPUT=/$WORKSPACE/job
 
 # urls
 export stream_url=https://hvobvxe805.execute-api.us-east-1.amazonaws.com
